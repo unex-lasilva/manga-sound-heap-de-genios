acd844c8ef62fa1bbd448fa5d51b9d8958b0faa0
Janna324 <janna1.jana@gmail.com>
2025-04-21 18:51:24 -0300

    adicionando conteúdo salvo da classe

--- manga-sound-player/src/main/java/br/com/mangarosa/repositoriomusical/ListaReproducao.java
+++ manga-sound-player/src/main/java/br/com/mangarosa/repositoriomusical/ListaReproducao.java
@@ -1,4 +1,51 @@
 package br.com.mangarosa.repositoriomusical;
 
+import javax.sound.sampled.Clip;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaReproducao {
+
+    private List<Clip> clipes;
+
+
+    public ListaReproducao() {
+        clipes = new ArrayList<>();
+    }
+
+
+    public void adicionarClip(Clip clip) {
+        clipes.add(clip);
+    }
+
+
+    public void removerClip(Clip clip) {
+        clipes.remove(clip);
+    }
+
+
+    public boolean isEmpty() {
+        return clipes.isEmpty();
+    }
+
+
+    public int size() {
+        return clipes.size();
+    }
+
+
+    public Clip getClip(int index) {
+        if (index >= 0 && index < clipes.size()) {
+            return clipes.get(index);
+        } else {
+            return null;
+        }
+    }
+
+
+    public void listarClipes() {
+        for (Clip clip : clipes) {
+            System.out.println(clip);
+        }
+    }
 }
