5ad237758f6c05e43eb5057ce8f205e8fbb91fc9
NanaGio <giovanna000gio@outlook.com>
2025-04-21 13:22:11 -0300

    correção das classes Musica e No de acordo com o diagrama.
    classes Musica e No estavam previamente erradas, corrigi de acordo com o diagrama.

--- manga-sound-player/src/main/java/br/com/mangarosa/model/No.java
+++ manga-sound-player/src/main/java/br/com/mangarosa/model/No.java
@@ -1,23 +1,27 @@
 package br.com.mangarosa.model;
 
 public class No {
-    private Musica musica;
-    private No proximo;
+    private Object valor;
+    private No prox;
 
-    public No(Musica musica) {
-        this.musica = musica;
-        this.proximo = null;
+    public No(Object valor, No prox) {
+        this.valor = valor;
+        this.prox = prox;
     }
 
-    public Musica getMusica() {
-        return musica;
+    public Object getValor() {
+        return valor;
     }
 
-    public No getProximo() {
-        return proximo;
+    public void setValor(Object valor) {
+        this.valor = valor;
     }
 
-    public void setProximo(No proximo) {
-        this.proximo = proximo;
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
     }
 }
