f64018c46dfab5372fff4f755315a3a52f6dffbb
Nana <giovanna000gio@outlook.com>
2025-04-23 08:41:41 -0300

    Classes Musica e No (#5)
    * classes main e no
    
    * correção das classes Musica e No de acordo com o diagrama.
    
    classes Musica e No estavam previamente erradas, corrigi de acordo com o diagrama.
    
    * Atualização no README.md
    
    Foram adicionados os nomes dos integrantes da equipe, nome da equipe e tabela de avaliação individual.
    
    * Update README.md
    
    ---------
    
    Co-authored-by: João Mira <jmirauniversitario@gmail.com>

--- manga-sound-player/src/main/java/br/com/mangarosa/model/Musica.java
+++ manga-sound-player/src/main/java/br/com/mangarosa/model/Musica.java
@@ -1,6 +1,50 @@
 package br.com.mangarosa.model;
 
 public class Musica {
+
+    private String titulo;
+    private int duracao;
+    private String path;
+    private String artista;
+
+    public Musica(String titulo, int duracao, String path, String artista) {
+        this.titulo = titulo;
+        this.duracao = duracao;
+        this.path = path;
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+      
     private String nome;
     private String caminho;
 
@@ -20,5 +64,6 @@ public class Musica {
     @Override
     public String toString() {
         return nome + " (" + caminho + ")";
+
     }
 }
